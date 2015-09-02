30dbdbaca09bc3107be5e93d09114986d1fb9f38
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-09-01 23:05:32 -0400

    It can set the file, but doesn't fool with permissions....yet

--- src/io/github/coursesplus/installer/CoursesPlusInstaller.java
+++ src/io/github/coursesplus/installer/CoursesPlusInstaller.java
@@ -2,11 +2,13 @@ package io.github.coursesplus.installer;
 
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GraphicsDevice;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -26,8 +28,11 @@ public class CoursesPlusInstaller extends JFrame implements ActionListener {
 	public static Font font;
 	public static Font bigFont;
 	public static Font titleFont;
-	
-	public static final String EXT_FILE = "";
+
+	public static final String BASE_PATH = "/Users/student/Library/Application Support/Google/Chrome/";
+	public static final String FOLDER_NAME = "External Extensions";
+	public static final String EXT_ID = "pieincmodljnbihihjnapcmhdddhbpgi";
+	public static final String EXT_FILE = "{\"external_update_url\": \"https://clients2.google.com/service/update2/crx\"}";
 	
 	public CoursesPlusInstaller() {
 		super("CoursesPlus Installer v" + VERSION);
@@ -84,7 +89,23 @@ public class CoursesPlusInstaller extends JFrame implements ActionListener {
 		switch (text) {
 			case "Install":
 				JOptionPane.showMessageDialog(null, "To continue, please close all open Chrome windows.", "Close Chrome windows", JOptionPane.WARNING_MESSAGE);
-				getGraphicsConfiguration().getDevice().setFullScreenWindow(this);
+				//getGraphicsConfiguration().getDevice().setFullScreenWindow(this);
+				File destDir = new File(BASE_PATH + FOLDER_NAME);
+				if (!destDir.exists()) {
+					destDir.mkdir();
+					destDir = new File(BASE_PATH + FOLDER_NAME);
+				}
+				try {
+					PrintWriter writer = new PrintWriter(BASE_PATH + FOLDER_NAME + "/" + EXT_ID + ".json", "UTF-8");
+					writer.write(EXT_FILE);
+					writer.close();
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (UnsupportedEncodingException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				break;
 				
 			default:
